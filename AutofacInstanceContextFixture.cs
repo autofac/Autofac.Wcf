99e4a394b97d027064851275d864ba104a3ef74b
travis.illig@gmail.com <unknown@kiln.example.com>
2012-05-10 22:02:17 +0700

    Moved core test assemblies to new location.

---
+++ AutofacInstanceContextFixture.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Autofac.Builder;
+using Autofac.Integration.Wcf;
+using NUnit.Framework;
+using Autofac.Util;
+using Autofac.Core;
+
+namespace Autofac.Tests.Integration.Wcf
+{
+	[TestFixture]
+	public class AutofacInstanceContextFixture
+	{
+		private class DisposeTracker : Disposable
+		{
+			public bool IsDisposedPublic
+			{
+				get
+				{
+					return this.IsDisposed;
+				}
+			}
+		}
+
+		[Test]
+		public void InstancesDisposed()
+		{
+			var builder = new ContainerBuilder();
+			builder.RegisterType<DisposeTracker>();
+			var container = builder.Build();
+			IComponentRegistration registration;
+			container.ComponentRegistry.TryGetRegistration(new TypedService(typeof(DisposeTracker)), out registration);
+			var context = new AutofacInstanceContext(container);
+			var disposable = (DisposeTracker)context.ResolveComponent(registration, Enumerable.Empty<Parameter>());
+			Assert.IsFalse(disposable.IsDisposedPublic);
+			context.Dispose();
+			Assert.IsTrue(disposable.IsDisposedPublic);
+		}
+	}
+}
