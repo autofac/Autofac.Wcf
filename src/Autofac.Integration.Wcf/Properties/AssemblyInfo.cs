c6963ff79f173a53a444e040431121656275a9cf
Travis Illig <tillig@paraesthesia.com>
2015-09-10 12:03:27 -0700

    Revert "Revert "Adding assembly info attributes back to AssemblyInfo to see if patching will work.""
    This reverts commit 22270ea833c3ffc5f35bea0d3fe3d502b2bf6405.

--- src/Autofac.Integration.Wcf/Properties/AssemblyInfo.cs
+++ src/Autofac.Integration.Wcf/Properties/AssemblyInfo.cs
@@ -11,3 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyCopyright("Copyright © 2014 Autofac Contributors")]
 [assembly: AssemblyDescription("Autofac Inversion of Control container WCF application integration.")]
+
+[assembly: AssemblyVersionAttribute("4.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("4.0.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("4.0.0.0")]
