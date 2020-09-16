b14db71af2a738f299d38492b4d5a7c2859cb3d7
alexmg <alex.meyergleaves@gmail.com>
2020-09-17 01:56:15 +1000

    Fix copyright header

--- src/Autofac.Integration.Wcf/AutofacInstanceProvider.cs
+++ src/Autofac.Integration.Wcf/AutofacInstanceProvider.cs
@@ -1,6 +1,4 @@
-﻿// This software is part of the Autofac IoC container
-// Copyright © 2011 Autofac Contributors
-// Copyright (c) Autofac Project. All rights reserved.
+﻿// Copyright (c) Autofac Project. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
